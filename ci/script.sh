c9c5ea066a3e07eee645593d257e8690d40d0fa2
Ilya Epifanov <Ilya_Epifanov@epam.com>
2017-07-18 22:17:13 +0200

    svd file is already generated manually, no need to generate it twice

--- ci/script.sh
+++ ci/script.sh
@@ -1,13 +1,13 @@
 set -ex
 
 main() {
-    local svd=freescale/mkw41z512xxx4/MKW41Z4.svd
+#    local svd=freescale/mkw41z512xxx4/MKW41Z4.svd
 
-    local url=http://ds.arm.com/media/resources/db/chip/$svd
-    local svd=$(basename $svd)
+#    local url=http://ds.arm.com/media/resources/db/chip/$svd
+#    local svd=$(basename $svd)
 #    curl -LO $url
-    svd2rust -i $svd | dos2unix | rustfmt > src/lib.rs
-    rm $svd
+#    svd2rust -i $svd | dos2unix | rustfmt > src/lib.rs
+#    rm $svd
 
     if [ "$TARGET" = "x86_64-unknown-linux-gnu" ]; then
         cargo check
